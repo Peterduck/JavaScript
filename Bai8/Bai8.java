c91a5baf614577236699450c3652d642a12d5c3a
Peterduck <you@example.com>
2023-08-20 17:06:53 +0700

    Bai10

---
+++ Bai8/Bai8.java
@@ -0,0 +1,10 @@
+package Bai8;
+
+public class Bai8 {
+  public static void main(String[] args){
+    //Viết chương trình JavaScript/Java trong đó chương trình lấy một số nguyên ngẫu nhiên trong khoảng từ 1 đến 10,
+    // sau đó người dùng được nhắc nhập một số dự đoán. Chương trình sẽ hiển thị thông báo "Làm tốt lắm" nếu đầu vào khớp với số dự đoán
+    // nếu không thì "Không khớp".
+  }
+
+}
